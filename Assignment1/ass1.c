9d0808827aaefb6fc4aa84020b55cac729c8b6ea
ravendasun <122367717+ravendasun@users.noreply.github.com>
2023-12-04 10:56:23 +0530

    Pset6 uploaded

--- Assignment1/ass1.c
+++ Assignment1/ass1.c
@@ -8,53 +8,69 @@ int main()
     
     int stampValue,tenderedAmount,dispensedStamp,change  ;
     int coins[] = {50, 25, 10, 5};
-    int chngcoins[] = {0, 0, 0, 0};
+    int coinCount[] = {5, 5, 5, 5};
+    int stampCount[] = {5, 5, 5};
+    int chngCoins[] = {0, 0, 0, 0};
+    char userinp[1];
 
     
     
 
-    printf("\nEnter the amount of tendering: ");
-    scanf("%d", &tenderedAmount);
-
-    printf("\nEnter the value of the stamp (50, 25, 10): ");
-    scanf("%d" , &stampValue);
-
-    if (stampValue != 50 && stampValue != 25 && stampValue != 10)
+    while (1>0)
     {
-        printf("\nError! Invalid Stamp value entered\nCoin returned.");
-    }
-    else if (tenderedAmount < stampValue)
-    {
-        printf("\nError! Insufficient amount tendered\nCoin returned.");
-        
-    }
-    else if (tenderedAmount != 50 && tenderedAmount != 25 && tenderedAmount != 10 && tenderedAmount != 5)
-    {
-        printf("\nError! Invalid Coin tendered.\nCoin returned.");
-    }
-    else
-    {
-        dispensedStamp = stampValue;
-        change = (tenderedAmount - stampValue);
+        printf("\nEnter the amount of tendering: ");
+        scanf("%d", &tenderedAmount);
 
-        printf("\n\nA Rs.%d.00 stamp has been dispensed\n", dispensedStamp);
+        printf("\nEnter the value of the stamp (50, 25, 10): ");
+        scanf("%d", &stampValue);
 
-        printf("\nYour change is Rs.%d.00\n", change);
-
-        
-        for (int i = 0; i < 4; i++)
+        if (stampValue != 50 && stampValue != 25 && stampValue != 10)
         {
-            chngcoins[i] = change/coins[i];
-            change %= coins[i];
-            
-            if (chngcoins[i] > 0)
-            {
-                printf("\nChange from %d coins : %d", coins[i], chngcoins[i]);
-            }
-            
+            printf("\nError! Invalid Stamp value entered\nCoin returned.");
         }
+        else if (tenderedAmount < stampValue)
+        {
+            printf("\nError! Insufficient amount tendered\nCoin returned.");
+        }
+        else if (tenderedAmount != 50 && tenderedAmount != 25 && tenderedAmount != 10 && tenderedAmount != 5)
+        {
+            printf("\nError! Invalid Coin tendered.\nCoin returned.");
+        }
+        else
+        {
+            dispensedStamp = stampValue;
+            change = (tenderedAmount - stampValue);
+
+            printf("\n\nA Rs.%d.00 stamp has been dispensed\n", dispensedStamp);
+
+            printf("\nYour change is Rs.%d.00\n", change);
+
+            for (int i = 0; i < 4; i++)
+            {
+                chngCoins[i] = change / coins[i];
+                change %= coins[i];
+
+                if (chngCoins[i] > 0)
+                {
+                    printf("\nChange from %d coins : %d", coins[i], chngCoins[i]);
+                }
+            }
+        }
+
+       printf("\n\nDo you want to exit? (y/n) : ") ;
+       scanf("%s", &userinp);
+
+       if (userinp[0] == 121)
+       {
+        printf("\nVender turning off.....");
+        break;
+       }
+       
+
         
+
     }
+    
     
 
 
--- pset6.c
+++ pset6.c
@@ -6,8 +6,61 @@
 int main()
 {
     
+    const int L1CA = 1000;
+    const int L1EA = 500;
+    const int L2CA = 750;
+    const int L2EA = 200;
+    const int L3CA = 500;
+    const int L3EA = 100;
+    const int L4CA = 250;
+    const int L4EA = 0;
+
+    int jobnum, levelnum, basicsal, grosssal, netsal;
+
+
+
+    printf("Enter <job number> <level number> <basic salary> : ");
+    scanf("%d %d %d", &jobnum, &levelnum, &basicsal);
+
+
+    if (levelnum == 1)
+    {
+        grosssal = basicsal + (0.25*basicsal) + L1CA + L1EA;
+    }
+    else if (levelnum == 2)
+    {
+        grosssal = basicsal + (0.25 * basicsal) + L2CA + L2EA;
+    }
+    else if (levelnum == 3)
+    {
+        grosssal = basicsal + (0.25 * basicsal) + L3CA + L3EA;
+    }
+    else if (levelnum == 4) 
+    {
+        grosssal = basicsal + (0.25 * basicsal) + L4CA + L4EA;
+    }
+  
+
+
+    if (grosssal < 2000)
+    {
+        netsal = grosssal;
+    }
+    else if (grosssal >= 2000 && grosssal < 4000)
+    {
+        netsal = grosssal - grosssal*0.03;
+    }
+    else if (grosssal >= 4000 && grosssal < 6000)
+    {
+        netsal = grosssal - grosssal*0.05;
+    }
+    else if (grosssal >= 6000)
+    {
+        netsal = grosssal - grosssal*0.08;
+    }
+
     
-    
+    printf("%d Net-Salary : Rs: %d.00", jobnum, netsal);
 
 
     return 0;
