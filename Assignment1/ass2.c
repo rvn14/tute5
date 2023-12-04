d1d91b6f7fd3d2edbde0146db33834b0c4fd09d5
ravendasun <122367717+ravendasun@users.noreply.github.com>
2023-12-04 18:36:43 +0530

    ass2 uploaded

--- Assignment1/ass2.c
+++ Assignment1/ass2.c
@@ -5,8 +5,23 @@
 
 int main()
 {
+
+    int table[6][8];
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("\nEnter %dst line: ",(i+1));
+        scanf("%d %d %d %d %d %d %d %d", table[i][0], table[i][1], table[i][2], table[i][3], table[i][4], table[i][5], table[i][6], table[i][7]);
+
+
+
+
+
+
+
+    }
     
-    
+    printf("fsdfsdfs");
 
     return 0;
 }
